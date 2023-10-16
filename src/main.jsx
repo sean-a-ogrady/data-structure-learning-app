f906237e66b3143b63d195994a7c5a578cc1ab9e
TymurBondar <tymurbondar@outlook.com>
2023-10-16 15:59:54 -0400

    Vite create basic app

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Hello</p>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
