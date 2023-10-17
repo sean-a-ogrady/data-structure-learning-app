fc1ee6592210e01ca89642665149303851c02033
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-10-17 09:22:45 -0400

    Implement dynamic routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import AddStructureForm from "./routes/addStructureForm";
 import StructuresList from "./routes/structuresList";
+import DataStructure from "./dataStructure";
 
 const url = "http://localhost:3000/structures";
 
@@ -19,7 +20,18 @@ const router = createBrowserRouter([
       {
         path: "/data-structures",
         element: <StructuresList />,
-        loader: async () => fetch(url).then(res => res.json())
+        loader: async () => fetch(url).then(res => res.json()),
+        children: [
+          {
+            path: ":structureName",
+            element: <DataStructure />,
+            loader: async ({params}) => {
+              const structureData = await fetch(`${url}/${params.structureName}`)
+                .then(res => res.json());
+              return structureData;
+            }
+          }
+        ]
       }
     ],
   },
