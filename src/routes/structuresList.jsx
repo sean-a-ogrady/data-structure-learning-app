454be50c9875df69840be1769f7b4f51a627199e
DennisShin <dennisshin0@gmail.com>
2023-10-18 10:59:30 -0400

    change styling

--- src/routes/structuresList.jsx
+++ src/routes/structuresList.jsx
@@ -6,7 +6,7 @@ export default function StructuresList(){
     return(
         <div className="flex mt-16" >
 
-            <ul className="ml-6 border-solid border-2 rounded border-slate-500  h-min" >
+            <ul className="flex-none ml-6 border-solid border-2 rounded border-slate-500  h-min" >
                 <h1 className="text-xl text-center font-extrabold pl-2 pr-2">Data Structures</h1>
                 {structures.map(structure => (
                     <li className='text-slate-950 text-lg text-center bg-slate-300' key={structure.name}>
