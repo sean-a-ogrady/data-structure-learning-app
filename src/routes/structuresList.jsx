377f1fa5c1ffc416518b6137979366c249c52544
DennisShin <dennisshin0@gmail.com>
2023-10-19 10:11:40 -0400

    add sticky side menu

--- src/routes/structuresList.jsx
+++ src/routes/structuresList.jsx
@@ -7,7 +7,7 @@ export default function StructuresList(){
     return(
         <div className="flex" >
 
-            <ul className="ml-5 h-screen border-solid border-r-2 dark:text-complimentary-dark" >
+            <ul className="ml-5 h-min sticky top-5 border-solid border-r-2 dark:text-complimentary-dark" >
                 <h1 className="text-3xl text-center font-extrabold mr-5 pr-2 mb-3 dark:text-red-dark">Data Structures</h1>
                 {structures.map(structure => (
                     <li className=' ml-1 text-2xl font-semibold text-left mr-5' key={structure.name}>
